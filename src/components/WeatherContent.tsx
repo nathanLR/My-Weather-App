3d68c46da7ebf6cb5aaa6fc378a216f2a1499f81
nathanLR <nathan.leroux3@gmail.com>
2022-07-07 09:59:46 +0200

    2 files modified, 2 added

--- src/components/WeatherContent.tsx
+++ src/components/WeatherContent.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { Divider } from 'antd';
 import WeatherForm from './WeatherForm';
+import WeatherData from './WeatherData';
+
+import {API_KEY, API_URL} from "../Utils";
 
 const WeatherContent = () => {
+  const [weatherData, setWeatherData] = useState<{}>();
+  useEffect(() => {
+    fetch(`${API_URL}lat=50&long=1&appid=${API_KEY}`)
+    .then(reponse => reponse.json())
+    .then(data => setWeatherData(data))
+  },[])
+  console.log(weatherData)
   return (
     <div>
       <Divider orientation="left">Localistation</Divider>
       <WeatherForm />
+      <WeatherData />
     </div>
   );
 };
--- src/components/WeatherForm.tsx
+++ src/components/WeatherForm.tsx
@@ -2,24 +2,40 @@ import React, { useState } from 'react';
 
 import { Form, Button, Input } from 'antd';
 
-
 type Coords = {
   lat: string;
   long: string;
 };
 
 const WeatherForm = () => {
-  const initialValue: {cityName: string, coords: Coords} = {cityName: "", coords: {lat: "", long: ""}}
-  const [localisation, setLocalisation] = useState(initialValue)
+  const initialValue: { cityName: string; coords: Coords } = {
+    cityName: '',
+    coords: { lat: '', long: '' },
+  };
+  const [localisation, setLocalisation] = useState(initialValue);
 
   const onFinish = (value: any) => {
     console.log(value);
   };
 
-  const handleCityChange = ({target}) => {
-    setLocalisation((prev) => {...prev, cityName: })
-  }
-
+  const handleCityChange = ({ target }) => {
+    setLocalisation((prev) => {
+      return { ...prev, cityName: target.value };
+    });
+  };
+  const handleCoordsChange = ({ target }) => {
+    const { name, value } = target;
+    setLocalisation((prev) => {
+      return {
+        ...prev,
+        coords: {
+          ...prev.coords,
+          [name]: value,
+        },
+      };
+    });
+  };
+  console.log(localisation);
   return (
     <Form
       onFinish={onFinish}
@@ -29,19 +45,18 @@ const WeatherForm = () => {
     >
       <Form.Item label="City Name">
         <Input
-        
           placeholder="ex: London, UK"
           value={localisation.cityName}
-          onChange={}
+          onChange={handleCityChange}
         />
       </Form.Item>
 
       <Form.Item label="Lat">
         <Input
-          placeholder="ex: 50.50"        
-          name="lat" 
+          placeholder="ex: 50.50"
+          name="lat"
           value={localisation.coords.lat}
-          onChange={}      
+          onChange={handleCoordsChange}
         />
       </Form.Item>
       <Form.Item label="Long">
@@ -49,7 +64,7 @@ const WeatherForm = () => {
           placeholder="ex: -0.12"
           name="long"
           value={localisation.coords.long}
-          onChange={}
+          onChange={handleCoordsChange}
         />
       </Form.Item>
 
