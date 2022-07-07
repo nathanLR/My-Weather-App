09a86860bac43cd360f20acd7a68ac73995b6ff1
nathanLR <nathan.leroux3@gmail.com>
2022-07-07 12:22:38 +0200

    1 file modified

--- src/components/WeatherContent.tsx
+++ src/components/WeatherContent.tsx
@@ -1,18 +1,18 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { Divider } from 'antd';
 import WeatherForm from './WeatherForm';
 import WeatherData from './WeatherData';
 
-import {API_KEY, API_URL} from "../Utils";
+import { API_KEY, API_URL } from '../Utils';
 
 const WeatherContent = () => {
   const [weatherData, setWeatherData] = useState<{}>();
   useEffect(() => {
     fetch(`${API_URL}lat=50&long=1&appid=${API_KEY}`)
-    .then(reponse => reponse.json())
-    .then(data => setWeatherData(data))
-  },[])
-  console.log(weatherData)
+      .then((reponse) => reponse.json())
+      .then((data) => setWeatherData(data));
+  }, []);
+
   return (
     <div>
       <Divider orientation="left">Localistation</Divider>
